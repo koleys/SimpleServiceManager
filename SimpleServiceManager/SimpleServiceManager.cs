32cda96431b5bdf545a5363507008acedbb50c48
skoley <satyarupkoley@gmail.com>
2025-01-18 16:25:48 +0530

    Add TestConsoleApp project and enhance process handling
    Updated SimpleServiceManager.sln to include TestConsoleApp project.
    Added GetAppParams and StartProcess methods in SimpleServiceManager.cs.
    Modified ExecuteAsync to use StartProcess and handle auto-restart.
    Updated appsettings.json with new configuration settings.
    Added Program.cs and TestConsoleApp.csproj for TestConsoleApp project.

--- SimpleServiceManager/SimpleServiceManager.cs
+++ SimpleServiceManager/SimpleServiceManager.cs
@@ -12,6 +12,21 @@ namespace ServiceWrapper
             _logger = logger;
             Configuration = (IConfigurationRoot)config;
         }
+        private string GetAppParams()
+        {
+            string retval = null;
+            string tempath = Configuration.GetSection("Configs:AppParams").Value.ToString();
+            if (!String.IsNullOrEmpty(tempath))
+            {
+                retval = tempath;
+            }
+            else
+            {
+                _logger.LogInformation("no AppParams : {time}", DateTimeOffset.Now);
+            }
+            return retval;
+        }
+
         private string GetPath()
         {
             string retval = null;
@@ -44,7 +59,41 @@ namespace ServiceWrapper
         {
             return File.Exists(path);
         }
+        private static void StartProcess(string filePath, string appParams)
+        {
+            string fileExtension = Path.GetExtension(filePath);
+            if (fileExtension.ToLower() == ".ps1")
+            {
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-File \"{filePath}\" {appParams}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
 
+                using (var process = new Process { StartInfo = processInfo })
+                {
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();                    
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(appParams))
+                {
+                    Process.Start(filePath, appParams);
+                }
+                else
+                {
+                    Process.Start(filePath);
+                }
+            }
+        }
         // Executes the service asynchronously, starting a process defined by the file path obtained from GetPath. 
         // The process is monitored by checking if it is running every second. 
         // If the process stops, the service is also stopped. 
@@ -56,7 +105,11 @@ namespace ServiceWrapper
 #endif
             Process myproc;
             string filePath = GetPath();
-            string fileName = GetFileName(filePath);
+            string appParams = GetAppParams();
+            //string fileName = GetFileName(filePath);
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            bool restartAppAutomatically = bool.Parse(Configuration.GetSection("Configs:RestartAppAutomatically").Value.ToString());
+            int restartDelay = int.Parse(Configuration.GetSection("Configs:RestartDelay").Value.ToString());
 
             // Add null check for filePath and fileName
             if (filePath == null || fileName == null)
@@ -67,7 +120,7 @@ namespace ServiceWrapper
             _logger.LogInformation("Simple Service Manager starting");
             try
             {
-                Process.Start(filePath);
+                StartProcess(filePath, appParams);
                 await Task.Delay(1000, stoppingToken);//1 sec delay before looping
                 _logger.LogInformation("Simple Service Manager started running at: {time}", DateTimeOffset.Now);
 
@@ -87,6 +140,12 @@ namespace ServiceWrapper
                     }
                     else
                     {
+                        if(restartAppAutomatically)
+                        {
+                            await Task.Delay(TimeSpan.FromMilliseconds(restartDelay));
+                            StartProcess(filePath, appParams);
+                            continue;
+                        }
                         Token.mytoken.Cancel();
                         _logger.LogInformation("Process stopped");
                         break; //"fileName" process stopped so service is also stopped
@@ -108,5 +167,6 @@ namespace ServiceWrapper
             }
         }
 
+        
     }
 }
